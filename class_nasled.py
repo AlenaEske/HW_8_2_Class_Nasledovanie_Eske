c45191e214e9f54fc6cb03663f6eb2bdace23958
Alena Eske <alenaeske86@gmail.com>
2024-05-31 08:22:30 +0300

    Создан класс Teacher

---
+++ class_nasled.py
@@ -0,0 +1,5 @@
+class Teacher:
+    def __init__(self, name_teacher, education, experience):
+        self._name_teacher = name_teacher
+        self._education = education
+        self._experience = experience
