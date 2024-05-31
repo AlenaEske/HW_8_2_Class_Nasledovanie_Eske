cfdc624c4e736b00ecd2a036d98b96615e88f1a9
Alena Eske <alenaeske86@gmail.com>
2024-05-31 08:23:29 +0300

    Написаны get и set для переменных класса Teacher

--- class_nasled.py
+++ class_nasled.py
@@ -3,3 +3,16 @@ class Teacher:
         self._name_teacher = name_teacher
         self._education = education
         self._experience = experience
+
+    def get_name_teacher(self):
+        return self._name_teacher
+
+    def get_education(self):
+        return self._education
+
+
+    def get_experience(self):
+        return self._experience
+
+    def set_experience(self, experience):
+        self._experience = experience
